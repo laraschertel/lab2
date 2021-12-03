6d4cc4e4874a428db4acc6c9b5792012fc800a69
lrnbr21 <laraschertel@gmail.com>
2021-12-03 14:41:49 +0100

    json file works

--- src/javascript/staff-page.js
+++ src/javascript/staff-page.js
@@ -1,102 +1,43 @@
 let studentList = document.querySelector('#studentList')
 
-let informatikStudents = [
-    {
-        studentID: 1,
-        firstName: "Iara",
-        lastName: "Neubauer",
-        dob: new Date('1998-11-08'),
-        gender: 'fem',
-        department: 'Fachbereich 4',
-        email: 'lala@email.com'
-    },
-    {
-        studentID: 2,
-        firstName: "Iuiu",
-        lastName: "Schertel",
-        dob: new Date('1998-08-11'),
-        gender: 'fem',
-        department: 'Fachbereich 2',
-        email: 'lulu@email.com'
-    }
-]
-let designStudents = [
-    {
-        studentID: 1,
-        firstName: "Dara",
-        lastName: "Neubauer",
-        dob: new Date('1998-11-08'),
-        gender: 'fem',
-        department: 'Fachbereich 4',
-        email: 'lala@email.com'
-    },
-    {
-        studentID: 2,
-        firstName: "Dudud",
-        lastName: "Schertel",
-        dob: new Date('1998-08-11'),
-        gender: 'fem',
-        department: 'Fachbereich 2',
-        email: 'lulu@email.com'
-    }
-]
-let medicineStudents = [
-    {
-        studentID: 1,
-        firstName: "Mara",
-        lastName: "Neubauer",
-        dob: new Date('1998-11-08'),
-        gender: 'fem',
-        department: 'Fachbereich 4',
-        email: 'lala@email.com'
-    },
-    {
-        studentID: 2,
-        firstName: "Mumu",
-        lastName: "Schertel",
-        dob: new Date('1998-08-11'),
-        gender: 'fem',
-        department: 'Fachbereich 2',
-        email: 'lulu@email.com'
-    }
-]
-let lawStudents = [
-    {
-        studentID: 1,
-        firstName: "Lara",
-        lastName: "Neubauer",
-        dob: new Date('1998-11-08'),
-        gender: 'fem',
-        department: 'Fachbereich 4',
-        email: 'lala@email.com'
-    },
-    {
-        studentID: 2,
-        firstName: "Lulu",
-        lastName: "Schertel",
-        dob: new Date('1998-08-11'),
-        gender: 'fem',
-        department: 'Fachbereich 2',
-        email: 'lulu@email.com'
-    }
-]
-let headers = ['Student ID', 'First Name', 'Last Name', 'DOB', 'Gender', 'Department', 'Email']
 
-function showSelectedList() {
+fetch('./studentList.json')
+    .then(response => response.json())
+    .then(data => students = data)
+    .then(json => console.log(json))
+
+
+let headers = ['Student ID', 'First Name', 'Last Name','Gender','DOB', 'Department', 'Email', "Joining Date"]
+
+
+function showSelectedListDep() {
     let courseSelection = document.getElementById("courseOptions");
     let selectedCourse = courseSelection.options[courseSelection.selectedIndex].text;
     studentList.innerHTML="";
     switch (selectedCourse) {
         case("Selectcourse"): break;
-        case("Informatik"): showStudentList(informatikStudents); break;
-        case("Design"): showStudentList(designStudents);break;
-        case("Medicine"): showStudentList(medicineStudents);break;
-        case("Law"): showStudentList(lawStudents);break;
+        case("F1"): showStudentListDep("F1"); break;
+        case("F2"): showStudentListDep("F2");break;
+        case("F3"): showStudentListDep("F3");break;
+        case("F4"): showStudentListDep("F4");break;
     }
 
 }
 
-function showStudentList(students) {
+function showSelectedListSem() {
+    let semesterSelection = document.getElementById("semesterOptions");
+    let selectedSemester = semesterSelection.options[semesterSelection.selectedIndex].text;
+    studentList.innerHTML="";
+    switch (selectedSemester) {
+        case("Selectsemester"): break;
+        case("Winter"): showStudentListSem("Winter"); break;
+        case("Sommer"): showStudentListSem("Sommer");break;
+
+    }
+
+}
+
+function showStudentListSem(Semester) {
     let table = document.createElement('table');
     let headerRow = document.createElement('tr');
 
@@ -106,9 +47,18 @@ function showStudentList(students) {
         header.appendChild(textNode);
         headerRow.appendChild(header);
     });
+
     table.appendChild(headerRow);
 
-    students.forEach(student => {
+    let studentsfilter;
+
+    if(Semester == "Sommer"){
+        studentsfilter = students.filter(stud => parseInt(stud.JoiningDate.split("/")[1]) >= 4 && stud.JoiningDate.split("/")[1] < 10);
+    } else {
+        studentsfilter = students.filter(stud => parseInt(stud.JoiningDate.split("/")[1]) < 4 || stud.JoiningDate.split("/")[1] >= 10);
+    }
+
+    studentsfilter.forEach(student => {
         let row = document.createElement('tr');
         Object.values(student).forEach(text => {
                 let cell = document.createElement('td');
@@ -121,6 +71,33 @@ function showStudentList(students) {
     })
 
     studentList.appendChild(table);
+}
 
+function showStudentListDep(Department) {
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+    students.filter(student => student.Department === Department).forEach(student => {
+        let row = document.createElement('tr');
+        Object.values(student).forEach(text => {
+                let cell = document.createElement('td');
+                let textNode = document.createTextNode(text);
+                cell.appendChild(textNode);
+                row.appendChild(cell);
+            }
+        )
+        table.appendChild(row);
+    })
+
+    studentList.appendChild(table);
 
 }
--- src/javascript/student-page.js
+++ src/javascript/student-page.js
@@ -8,51 +8,134 @@ function hideForm() {
 
 let studentList = document.querySelector('#studentList')
 
-let students = [
-    {
-        studentID: 1,
-        firstName: "Lara",
-        lastName: "Neubauer",
-        dob: new Date('1998-11-08'),
-        gender: 'fem',
-        department: 'Fachbereich 4',
-        email: 'lala@email.com'
-    },
-    {
-        studentID: 2,
-        firstName: "Lara",
-        lastName: "Schertel",
-        dob: new Date('1998-08-11'),
-        gender: 'fem',
-        department: 'Fachbereich 2',
-        email: 'lulu@email.com'
+var students
+
+fetch('./studentList.json')
+    .then(response => response.json())
+    .then(data => students = data)
+    .then(json => console.log(json))
+
+
+let headers = ['Student ID', 'First Name', 'Last Name','Gender','DOB', 'Department', 'Email', "Joining Date"]
+
+function showSelectedListDep() {
+
+    let courseSelection = document.getElementById("courseOptions");
+    let selectedCourse = courseSelection.options[courseSelection.selectedIndex].text;
+    studentList.innerHTML="";
+    switch (selectedCourse) {
+        case("Selectcourse"): break;
+        case("F1"): showStudentListDep("F1"); break;
+        case("F2"): showStudentListDep("F2");break;
+        case("F3"): showStudentListDep("F3");break;
+        case("F4"): showStudentListDep("F4");break;
     }
-]
 
-let headers = ['Student ID', 'First Name', 'Last Name', 'DOB', 'Gender', 'Department', 'Email']
+}
 
-let table = document.createElement('table');
-let headerRow = document.createElement('tr');
+function showSelectedListSem() {
+    let semesterSelection = document.getElementById("semesterOptions");
+    let selectedSemester = semesterSelection.options[semesterSelection.selectedIndex].text;
+    studentList.innerHTML="";
+    switch (selectedSemester) {
+        case("Selectsemester"): break;
+        case("Winter"): showStudentListSem("Winter"); break;
+        case("Sommer"): showStudentListSem("Sommer");break;
 
-headers.forEach(headerText => {
-    let header = document.createElement('th');
-    let textNode = document.createTextNode(headerText);
-    header.appendChild(textNode);
-    headerRow.appendChild(header);
-});
-table.appendChild(headerRow);
-
-students.forEach(student => {
-    let row = document.createElement('tr');
-    Object.values(student).forEach(text => {
-        let cell = document.createElement('td');
-        let textNode = document.createTextNode(text);
-        cell.appendChild(textNode);
-        row.appendChild(cell);
     }
-    )
-    table.appendChild(row);
-})
 
-studentList.appendChild(table);
+}
+
+function showStudentListSem(Semester) {
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+    let studentsfilter;
+
+    if(Semester == "Sommer"){
+        studentsfilter = students.filter(stud => parseInt(stud.JoiningDate.split("/")[1]) >= 4 && stud.JoiningDate.split("/")[1] < 10);
+    } else {
+        studentsfilter = students.filter(stud => parseInt(stud.JoiningDate.split("/")[1]) < 4 || stud.JoiningDate.split("/")[1] >= 10);
+    }
+
+    studentsfilter.forEach(student => {
+        let row = document.createElement('tr');
+        Object.values(student).forEach(text => {
+                let cell = document.createElement('td');
+                let textNode = document.createTextNode(text);
+                cell.appendChild(textNode);
+                row.appendChild(cell);
+            }
+        )
+        table.appendChild(row);
+    })
+
+    studentList.appendChild(table);
+}
+
+function showStudentListDep(Department) {
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(headerRow);
+
+    students.filter(student => student.Department === Department).forEach(student => {
+        let row = document.createElement('tr');
+        Object.values(student).forEach(text => {
+                let cell = document.createElement('td');
+                let textNode = document.createTextNode(text);
+                cell.appendChild(textNode);
+                row.appendChild(cell);
+            }
+        )
+        table.appendChild(row);
+    })
+
+    studentList.appendChild(table);
+
+}
+
+function checkDate() {
+    var selectedDate = document.getElementById("dob").value
+    var currDate = new Date()
+    var age = parseInt(currDate.getFullYear()) - parseInt(selectedDate.split("-")[0])
+    var month = parseInt(currDate.getMonth()) - parseInt(selectedDate.split("-")[1])
+    var day = parseInt(currDate.getDay()) - parseInt(selectedDate.split("-")[2])
+
+    if(age < 17 || age > 60){
+        alert('Invalid DOB')
+    } else if (age == 17) {
+        if (month < 0) {
+            alert('Invalid DOB')
+        } else if (month == 0) {
+            if (day < 0) {
+                alert('Invalid DOB')
+            }
+        }
+    }else if (age == 60) {
+        if (month > 0) {
+            alert('Invalid DOB')
+        } else if (month == 0) {
+            if (day > 0 || day == 0) {
+                alert('Invalid DOB')
+            }
+        }
+    }
+}
 
